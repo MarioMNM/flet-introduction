61020847b4537e4ee0fb892679f1e2e4294f597e
MarioMNM <mario.novella@gmail.com>
2023-10-10 22:28:19 +0200

    Refactored TodoApp class

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,31 +1,5 @@
 import flet as ft
-
-
-class TodoApp(ft.UserControl):
-    def build(self):
-        self.new_task = ft.TextField(hint_text="Add a new To-Do", expand=True)
-        self.tasks_view = ft.Column()
-
-        return ft.Column(
-            width=600,
-            controls=[
-                ft.Row(
-                    controls=[
-                        self.new_task,
-                        ft.FloatingActionButton(
-                            icon=ft.icons.ADD, 
-                            on_click=self.handleAddEvent
-                        ),
-                    ],
-                ),
-                self.tasks_view,
-            ],
-        )
-
-    def handleAddEvent(self, e):
-        self.tasks_view.controls.append(ft.Checkbox(label=self.new_task.value))
-        self.new_task.value = ""
-        self.update()
+from Todo import TodoApp
 
 
 def main(page: ft.Page):
